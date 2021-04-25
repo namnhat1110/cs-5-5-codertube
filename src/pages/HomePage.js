4afb7c2ed868714bc41f1240063ef4efc898ee24
Tien <ldtien.1993@gmail.com>
2021-04-25 12:29:23 +0700

    modified trending

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,17 +9,26 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
+  const [trendingM, setTrendingM] = useState([]);
 
+  const fetchTrendingM = async () => {
+    const response = await fetch(
+      `${URL_BACKEND}trending/movie/week?api_key=${API_KEY}`
+    );
+    const json = await response.json();
+    setTrendingM(json.results);
+  };
   const fetchData = async () => {
     const response = await fetch(
       `${URL_BACKEND}movie/now_playing?api_key=${API_KEY}`
     );
     const json = await response.json();
-    console.log({ json });
+
     setMovies(json.results);
   };
 
   useEffect(() => {
+    fetchTrendingM();
     fetchData();
   }, []);
 
@@ -27,48 +36,81 @@ const HomePage = () => {
     <div className="mt-3 container">
       <h3 style={{ color: "grey", textAlign: "left" }}>Trending</h3>
 
-      {movies.slice(0, 1).map((m) => {
+      {trendingM.slice(0, 1).map(() => {
         return (
           <Carousel fade className="container">
             <Carousel.Item>
               <img
                 className="d-block w-100 "
                 src={
-                  "https://image.tmdb.org/t/p/w500/" + movies[0].backdrop_path
+                  "https://image.tmdb.org/t/p/w500/" +
+                  trendingM[0].backdrop_path
                 }
                 alt="First slide"
               />
               <Carousel.Caption>
-                <h3>{movies[0].title}</h3>
-                <p className="small-text">{movies[0].overview}</p>
+                <h3>{trendingM[0].title}</h3>
+                <p className="small-text">{trendingM[0].overview}</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
                 className="d-block w-100 "
                 src={
-                  "https://image.tmdb.org/t/p/w500/" + movies[1].backdrop_path
+                  "https://image.tmdb.org/t/p/w500/" +
+                  trendingM[1].backdrop_path
                 }
                 alt="Second slide"
               />
 
               <Carousel.Caption>
-                <h3>{movies[1].title}</h3>
-                <p className="small-text">{movies[1].overview}</p>
+                <h3>{trendingM[1].title}</h3>
+                <p className="small-text">{trendingM[1].overview}</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
                 className="d-block w-100 "
                 src={
-                  "https://image.tmdb.org/t/p/w500/" + movies[2].backdrop_path
+                  "https://image.tmdb.org/t/p/w500/" +
+                  trendingM[2].backdrop_path
                 }
                 alt="Third slide"
               />
 
               <Carousel.Caption>
-                <h3>{movies[2].title}</h3>
-                <p className="small-text">{movies[2].overview}</p>
+                <h3>{trendingM[2].title}</h3>
+                <p className="small-text">{trendingM[2].overview}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 "
+                src={
+                  "https://image.tmdb.org/t/p/w500/" +
+                  trendingM[3].backdrop_path
+                }
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>{trendingM[3].title}</h3>
+                <p className="small-text">{trendingM[3].overview}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100 "
+                src={
+                  "https://image.tmdb.org/t/p/w500/" +
+                  trendingM[4].backdrop_path
+                }
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>{trendingM[4].title}</h3>
+                <p className="small-text">{trendingM[4].overview}</p>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
